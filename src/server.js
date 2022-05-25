967cf409ab93988ac3107044e13253ac36ccd25f
Maria Gabriela <mgvillarreal09@gmail.com>
2022-05-25 17:14:31 -0300

    actualizaciones heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'package.json')); //'/dist/ng-blog'
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'package.json'));
 });
 
 app.listen(process.env.PORT || 8080);
