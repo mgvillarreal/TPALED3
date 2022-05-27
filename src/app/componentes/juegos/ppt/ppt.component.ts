9b5cfdd22ff72d393ba81465f9f2d09aae6e0c93
Maria Gabriela <mgvillarreal09@gmail.com>
2022-05-27 14:26:47 -0300

    juego piedra papel o tijera

--- src/app/componentes/juegos/ppt/ppt.component.ts
+++ src/app/componentes/juegos/ppt/ppt.component.ts
@@ -6,9 +6,123 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ppt.component.css']
 })
 export class PptComponent implements OnInit {
+  
+  opJug:string;
+  opPc:string;
+  resultado:string = '';
+  puntajePc:number = 0;
+  puntajeJug:number = 0;
 
-  constructor() { }
+  constructor() {
+   
+  }
 
+  tomarOpcionJugador(opcion:string):void{
+    this.opJug = opcion;
+    this.jugar();
+  }
+
+  opcionPc(): void{
+    let opcionPc = Math.floor(Math.random() * 2 + 0);
+    
+    switch(opcionPc){
+      case 0:
+        this.opPc = 'Piedra';
+        break;
+      case 1:
+        this.opPc = 'Papel';
+        break;
+      case 2:
+        this.opPc = 'Tijera';
+    }
+  }
+ 
+
+  jugar(): void{
+    this.opcionPc();
+
+      switch(this.opPc){
+        case 'Piedra':
+          if(this.opJug == 'Papel')
+          {
+            this.resultado = 'Tú ganas';
+            this.puntajeJug++;
+          }
+          else
+          {
+            if(this.opJug == 'Tijera')
+            {
+              this.resultado = 'Computadora gana';
+              this.puntajePc++;
+            }
+            else
+            {
+              this.resultado = 'Empate';
+            }
+          }
+        break;
+        case 'Papel':
+          if(this.opJug == 'Papel')
+          {
+            this.resultado = 'Empate';
+          }
+          else
+          {
+            if(this.opJug == 'Tijera')
+            {
+              this.resultado = 'Tú ganas';
+              this.puntajeJug++;
+            }
+            else
+            {
+              this.resultado = 'Computadora gana';
+              this.puntajePc++;
+            }
+          }
+        break;
+        default: //Pc elige Tijera
+            if(this.opJug == 'Papel')
+          {
+            this.resultado = 'Computadora gana';
+            this.puntajePc++;
+          }
+          else
+          {
+            if(this.opPc == 'Tijera')
+            {
+              this.resultado = 'Empate';
+            }
+            else
+            {
+              this.resultado = 'Tú ganas';
+              this.puntajeJug++;
+            }
+          }
+        break;
+      }
+      this.validarResultado();
+  }
+
+  validarResultado():void{
+    if(this.puntajeJug == 3 || this.puntajePc == 3)
+    {
+      if(this.puntajeJug > this.puntajePc)
+      {
+        this.resultado = '¡Ganaste la partida!';
+      }
+      else
+      {
+        this.resultado = 'Perdiste la partida :(';
+      }
+    }
+  }
+
+  resetear():void{
+    this.resultado = '';
+    this.puntajePc = 0;
+    this.puntajeJug = 0;
+  }
+  
   ngOnInit(): void {
   }
 
