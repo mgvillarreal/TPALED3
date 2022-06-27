88cad57260073ebc6db7fee860f8292eab42f651
Maria Gabriela <mgvillarreal09@gmail.com>
2022-06-26 21:53:27 -0300

    inicio de ahorcado

--- src/app/componentes/juegos/ahorcado/ahorcado.component.ts
+++ src/app/componentes/juegos/ahorcado/ahorcado.component.ts
@@ -1,3 +1,4 @@
+import { UnaryOperator } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorcadoComponent implements OnInit {
 
-  constructor() { }
+  imagen:number = 1;
+  palabraSecreta:string;
+  palabraVisible:string = "";
+  palabraVisibleAct:string;
+  letra:string;
+  posicionLetra:number;
+
+
+  constructor() {
+    this.palabraSecreta = 'prueba';
+    this.crearPalabraVisible();
+  }
+
+  crearPalabraVisible():void{
+    for (let i = 0; i < this.palabraSecreta.length; i++) {
+      this.palabraVisible = this.palabraVisible.concat("_ ");
+    }
+  };
+
+  tomaLetra(letra: string){
+    this.letra = letra;
+    console.log("Letra seleccionada: ", this.letra);
+    this.jugar();
+  }
+
+  jugar():void{
+    if(this.palabraSecreta.indexOf(this.letra) != -1)
+    {
+      this.actualizaPalabraVisible();
+      // console.log("prueba: ", this.palabraSecreta.charAt(this.palabraSecreta.indexOf(this.letra)));
+    }
+    else
+    {
+      this.imagen++;
+    }
+  }
+
+  actualizaPalabraVisible():void{
+    // this.palabraVisibleAct = this.palabraVisible.replace(this.palabraVisible[this.posicionLetra], this.letra);
+    // console.log("Prueba reemplazo: ", this.palabraVisibleAct);
+    // this.palabraVisible = this.palabraVisibleAct;
+    let retoro:String = "";
+    for (let i = 0; i < this.palabraSecreta.length; i++)
+    {
+      if(this.palabraSecreta.charAt(i) == this.letra)
+      {
+        console.log("posicion char at: ", this.palabraSecreta.charAt(i));
+        retoro = retoro.concat(this.letra + " "); 
+      }
+      else
+      {
+        retoro = retoro.concat(this.palabraVisible.charAt(i) + " ");
+      }
+    }
+    console.log("Palabra Actu: ", retoro);
+  }
 
   ngOnInit(): void {
   }
