61bf97f6999ef114acb5eb031b2f284e41d9171b
Maria Gabriela <mgvillarreal09@gmail.com>
2022-06-27 21:18:51 -0300

    cambios ahorcado

--- src/app/componentes/juegos/ahorcado/ahorcado.component.ts
+++ src/app/componentes/juegos/ahorcado/ahorcado.component.ts
@@ -8,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorcadoComponent implements OnInit {
 
+  letras: string[] = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w','x','y','z']
+  tecladoActivado:number=1;
+  resultado:string;
   imagen:number = 1;
   palabraSecreta:string;
   palabraVisible:string = "";
   letra:string;
-
+  listaPalabras:string[] = ['guitarra', 'suciedad', 'abrelatas', 'periferia', 'pantalla', 'manantial', 'cerradura', 'claridad', 'mausoleo', 'creatividad', 'humareda', 'parabrisas', 'estigma', 'esclavitud', 'sobrenatural']
+  letrasJugadas:any = [];
 
   constructor() {
-    this.palabraSecreta = 'separar';
+    this.eligePalabra();
+  }
+
+  eligePalabra():void{
+    let opcion = Math.floor(Math.random() * (0 - 12) * -1);
+    this.palabraSecreta = this.listaPalabras[opcion];
     this.crearPalabraVisible();
   }
 
@@ -28,6 +37,17 @@ export class AhorcadoComponent implements OnInit {
   tomaLetra(letra: string){
     this.letra = letra;
     console.log("Letra seleccionada: ", this.letra);
+
+    // this.letrasJugadas.push(letra);
+    // console.log("letrass: ", this.letrasJugadas);
+    // for(let i = 0; i<this.letrasJugadas.length; i++)
+    // {
+    //   if(this.letrasJugadas[i] === letra)
+    //   {
+    //     this.resultado = "Ya jugaste la letra " + letra;
+    //   }
+    // }
+
     this.jugar();
   }
 
@@ -40,10 +60,11 @@ export class AhorcadoComponent implements OnInit {
     {
       this.imagen++;
     }
+    this.validaJugada();
   }
 
   actualizaPalabraVisible(letra: string):void{
-    let aux =[...this.palabraVisible];
+    let aux = [...this.palabraVisible];
 
     for(let i = 0; i<this.palabraSecreta.length; i++)
     {
@@ -54,7 +75,32 @@ export class AhorcadoComponent implements OnInit {
         this.palabraVisible = aux.join("");
       }
     }
+  }
 
+  validaJugada():void{
+
+    if(this.imagen === 7)
+    {
+      this.tecladoActivado = 0;
+      this.resultado = "Perdiste :(";
+    }
+    else
+    {
+      if(this.palabraVisible === this.palabraSecreta)
+      {
+        this.tecladoActivado = 0;
+        this.resultado = "Ganaste!!";
+      }
+    }
+  }
+
+  resetear(){
+    this.tecladoActivado = 1;
+    this.resultado = "";
+    this.imagen = 1;
+    this.letra = "";
+    this.palabraVisible = "";
+    this.eligePalabra();
   }
 
   ngOnInit(): void {
